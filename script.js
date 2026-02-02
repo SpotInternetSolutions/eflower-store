752c61149a9cab53250a40c9415b3daa085c2c9d
SpotInternetSolutions <info.spotinternetsolutions@gmail.com>
2026-02-02 12:57:21 +0300

    Initial edit

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+// USERS
+function register() {
+  let email = document.getElementById("regEmail").value;
+  let password = document.getElementById("regPassword").value;
+
+  localStorage.setItem("user", JSON.stringify({ email, password }));
+  alert("Account created. Please login.");
+  window.location.href = "login.html";
+}
+
+function login() {
+  let email = document.getElementById("loginEmail").value;
+  let password = document.getElementById("loginPassword").value;
+
+  let user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.email === email && user.password === password) {
+    alert("Login successful");
+    window.location.href = "index.html";
+  } else {
+    alert("Invalid login details");
+  }
+}
+
+// CART
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(name, price) {
   cart.push({ name, price });
   localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Product added to cart");
+  alert("Added to cart");
 }
-
-function loadCart() {
-  let cartDiv = document.getElementById("cart");
-  if (!cartDiv) return;
-
-  cartDiv.innerHTML = "";
-  cart.forEach(item => {
-    cartDiv.innerHTML += `<p>${item.name} - KES ${item.price}</p>`;
-  });
-}
-
-function clearCart() {
-  localStorage.removeItem("cart");
-  location.reload();
-}
-
-loadCart();
